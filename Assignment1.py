886282748c252647bf56a886bc6da2c29ea6d417
Nkhede <khedekarnakshatra@gmail.com>
2025-05-24 00:08:15 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+#Q1 Perfome Basic Mathematical operation using input
+A=int(input("Enter the First Number: "))
+B=int(input("Enter the Second Number: "))
+Addition=A+B
+Subtraction=B-A
+Multiplication=A*B
+Division=A/B
+
+print("Addition of A and B is:",Addition)
+print("Subtraction of B and A is:",Subtraction)
+print("Multiplication of A and B is:",Multiplication)
+print("Division of A and B is:",Division)
+
+#Q2 Create a personalized greetings
+name=input("Enter your name: ")
+lastName=input("Enter your last name: ")
+print(f'Hello {name},{lastName},Welcome to the Python program.')
